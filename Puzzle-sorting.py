4f250504c8e1db9c279c50544e5f9ff943860eb2
Mohammad <m.a.hajialirezaei05@gmail.com>
2025-05-28 13:50:31 +0330

    add base of bfs

--- Puzzle-sorting.py
+++ Puzzle-sorting.py
@@ -1,2 +1,17 @@
+from collections import deque
+
+
 def puzzle_sorting(puzzle):
     purpose = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    visited = set()
+    start = []
+
+    for x in puzzle:
+        for y in puzzle:
+            start.append(y)
+    puzzle_queue = deque(start)
+    visited.add(start)
+    step = 0
+
+
+
